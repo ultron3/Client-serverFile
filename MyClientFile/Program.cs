eca68fc9066eedfc3f3306170bcf0e64ed6ef87b
alex gnavi <104757961+ultron3@users.noreply.github.com>
2023-08-15 20:03:12 +0200

    Add files via upload

---
+++ MyClientFile/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Net.Sockets;
+
+class Client
+{
+    static void Main()
+    {
+        string serverIp = "192.168.1.114"; // Indirizzo IP del server
+        int serverPort = 5550;
+
+        using (TcpClient client = new TcpClient(serverIp, serverPort))
+        {
+            using (NetworkStream networkStream = client.GetStream())
+            {
+                Console.Write("Inserisci il percorso completo del file exe da inviare: ");
+                string filePath ="program.exe";
+                byte[] fileData = File.ReadAllBytes(filePath);
+                networkStream.Write(fileData, 0, fileData.Length);
+            }
+        }
+
+        Console.WriteLine("File inviato al server.");
+    }
+}
---
+++ MyServerFile/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+TcpListener listener = new TcpListener(IPAddress.Any, 5550);
+listener.Start();
+Console.WriteLine("Server avviato. In attesa di connessioni...");
+
+while (true)
+{
+    TcpClient client = listener.AcceptTcpClient();
+    Console.WriteLine("Cliente connesso.");
+
+    Thread clientThread = new Thread(HandleClient);
+    clientThread.Start(client);
+}
+
+void HandleClient(object clientObj)
+{
+    TcpClient client = (TcpClient)clientObj;
+
+    using (NetworkStream networkStream = client.GetStream())
+    {
+        byte[] buffer = new byte[1024];
+        int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+
+        string fileName = "program.exe"; // Nome temporaneo del file ricevuto
+        using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
+        {
+            fileStream.Write(buffer, 0, bytesRead);
+        }
+
+        // Simuliamo la trasformazione in .inv
+        string transformedFileName = Path.ChangeExtension(fileName, ".inv");
+        File.Move(fileName, transformedFileName);
+
+        Console.WriteLine("File trasformato in .inv: " + transformedFileName);
+
+        networkStream.Close();
+    }
+
+    client.Close();
+    Console.WriteLine("Cliente disconnesso.");
+}
