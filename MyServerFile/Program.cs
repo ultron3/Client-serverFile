23388c1d6dd1bb5e40f4886b3f349270a30d72dd
alex gnavi <104757961+ultron3@users.noreply.github.com>
2023-08-19 10:54:40 +0200

    Update Program.cs

--- MyServerFile/Program.cs
+++ MyServerFile/Program.cs
@@ -1,48 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
+
 using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Collections.Generic;
 
-
-TcpListener listener = new TcpListener(IPAddress.Any, 5550);
-listener.Start();
-Console.WriteLine("Server avviato. In attesa di connessioni...");
-
-while (true)
+public class Server
 {
-    TcpClient client = listener.AcceptTcpClient();
-    Console.WriteLine("Cliente connesso.");
+    static List<TcpClient> clients = new List<TcpClient>();
+    static TcpListener listener;
+    static int serverPort = 5550;
 
-    Thread clientThread = new Thread(HandleClient);
-    clientThread.Start(client);
-}
-
-void HandleClient(object clientObj)
-{
-    TcpClient client = (TcpClient)clientObj;
-
-    using (NetworkStream networkStream = client.GetStream())
+  public  static void Main(string[]args)
     {
-        byte[] buffer = new byte[1024];
-        int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+        listener = new TcpListener(IPAddress.Any, serverPort);
+        listener.Start();
+        Console.WriteLine("Server avviato. In attesa di connessioni...");
 
-        string fileName = "program.exe"; // Nome temporaneo del file ricevuto
-        using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
+        while (true)
         {
-            fileStream.Write(buffer, 0, bytesRead);
+            TcpClient client = listener.AcceptTcpClient();
+            clients.Add(client);
+            Console.WriteLine("Cliente connesso.");
+
+            Thread clientThread = new Thread(HandleClient);
+            clientThread.Start(client);
         }
-
-        // Simuliamo la trasformazione in .inv
-        string transformedFileName = Path.ChangeExtension(fileName, ".inv");
-        File.Move(fileName, transformedFileName);
-
-        Console.WriteLine("File trasformato in .inv: " + transformedFileName);
-
-        networkStream.Close();
     }
 
-    client.Close();
-    Console.WriteLine("Cliente disconnesso.");
+    static void HandleClient(object clientObj)
+    {
+        TcpClient client = (TcpClient)clientObj;
+
+        using (NetworkStream networkStream = client.GetStream())
+        {
+         try{
+
+         
+
+            
+            byte[] buffer = new byte[1024];
+            int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+
+            string fileName = "program.exe"; // Nome temporaneo del file ricevuto
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
+            {
+                fileStream.Write(buffer, 0, bytesRead);
+            }
+
+            // Simuliamo la trasformazione in .inv
+            string transformedFileName = Path.ChangeExtension(fileName, ".inv");
+            File.Move(fileName, transformedFileName);
+
+            Console.WriteLine("File trasformato in .inv: " + transformedFileName);
+         }
+         catch(IOException ex)
+         {
+             Console.WriteLine("Errore di lettura/scrittura: " + ex.Message);
+         }
+         finally{
+         networkStream.Close();
+            client.Close();
+            clients.Remove(client);
+            Console.WriteLine("Cliente disconnesso.");
+         }
+           
+        }
+
+        
+    }
+
+ public  static void SendFileToClients(string filePath)
+    {
+        byte[] fileData = File.ReadAllBytes(filePath);
+        
+        foreach (TcpClient client in clients)
+        {
+            using (NetworkStream networkStream = client.GetStream())
+            {
+                networkStream.Write(fileData, 0, fileData.Length);
+            }
+        }
+    }
 }
